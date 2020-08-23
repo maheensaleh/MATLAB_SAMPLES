cb614a4fffc42cc90e5f8ca575c3ad8ac412eac8
maheensaleh <maheensaleh@yahoo.com>
2020-08-23 16:18:47 +0500

    first

---
+++ w1a.m
@@ -0,0 +1,15 @@
+% create an array method 01:
+array1 = [1,2,3]
+
+% create an array method 02 : by defining range
+array2 = 1:2:20
+array3 = 1:10 % when no mid number means step =1 
+
+% ploting 
+
+x1 = 0:5;
+y1 = 0:2:10;
+% continuous plot
+figure,plot(x1,y1);
+% disc plot
+figure,stem(x1,y1);
